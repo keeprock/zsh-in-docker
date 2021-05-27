c10d4968d0a02b0b054a1a03ec034a5cfd58c505
Alex <5209820+keeprock@users.noreply.github.com>
2021-05-27 04:47:55 +0300

    Update zsh-in-docker.sh

--- zsh-in-docker.sh
+++ zsh-in-docker.sh
@@ -76,6 +76,7 @@ install_dependencies() {
         *)
             $Sudo apt-get update
             $Sudo apt-get -y install git curl zsh locales
+            $Sudo apt-get install fonts-powerline
             if [ "$VERSION" != "14.04" ]; then
                 $Sudo apt-get -y install locales-all
             fi
